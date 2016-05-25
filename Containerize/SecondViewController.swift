be83b63aaece31c243f86d232e4c092016c52da6
Roshan Ravi <roshanravi10@gmail.com>
2016-05-25 16:39:38 -0400

    Initial Commit

---
+++ Containerize/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Containerize
+//
+//  Created by Roshan Ravi on 5/25/16.
+//  Copyright © 2016 Roshan Ravi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
